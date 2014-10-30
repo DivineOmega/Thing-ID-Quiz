20841ad92a2cfe0522fbc6955997b32d2a0746ca
Jordan Hall <jordan.hall@rapidweb.biz>
2014-10-30 15:04:30 +0000

    Main functionality of game and several data lists

--- src/main/List.java
+++ src/main/List.java
@@ -1,6 +1,40 @@
 package main;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class List 
 {
-
+	private String name;
+	private ArrayList<String> words = new ArrayList<String>();
+	
+	public List(File file) throws IOException
+	{
+		name = file.getName().replace('_', ' ').replace(".txt", "").trim();
+		
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		String line;
+		while ((line = br.readLine()) != null) 
+		{
+		   if (line.trim().isEmpty()) continue;
+		   words.add(line.trim());
+		}
+		br.close();
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public String getRandomWord()
+	{
+		Collections.shuffle(words);
+		
+		return words.get(0);
+	}
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,111 @@
 package main;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+import com.sun.org.apache.bcel.internal.generic.LSTORE;
+
 public class Main 
 {
-
+	public static ArrayList<List> lists = new ArrayList<List>();
+	
 	public static void main(String[] args) 
 	{
+		File directory = new File("lists");
+		
+		File files[] = directory.listFiles();
+		
+		for (File file : files) 
+		{
+			try 
+			{
+				lists.add(new List(file));
+				System.out.println("Loaded list: "+file.getName());
+			} 
+			catch (IOException e) 
+			{
+				System.out.println("Error loading list ("+e+"): "+file.getName());
+			}
+		}
+		
+		System.out.println();
+		System.out.println("*** Welcome to Thing ID Quiz ***");
+		System.out.println();
+		
+		while(true)
+		{
+			Collections.shuffle(lists);
+			
+			List activeList = lists.get(0);
+			String activeWord = activeList.getRandomWord();
+			
+			String a_or_an = "a";
+			if (activeWord.charAt(0)=='a' || activeWord.charAt(0)=='e' || activeWord.charAt(0)=='i' || activeWord.charAt(0)=='o' || activeWord.charAt(0)=='u') a_or_an = "an";
+			
+			System.out.println("What is "+a_or_an+" "+activeWord+"?");
+						
+			ArrayList<List> listOptions = new ArrayList<List>();
+			
+			while(!listOptions.contains(activeList))
+			{
+				Collections.shuffle(lists);
+				
+				listOptions.clear();
+				listOptions.add(lists.get(0));
+				listOptions.add(lists.get(1));
+				listOptions.add(lists.get(2));
+			}
+			
+			System.out.println();
+			
+			int x = 1;
+			for (List list : lists) 
+			{
+				System.out.println("["+x+"] "+list.getName());
+				x++;
+				if (x>3) break;
+			}
+			
+			System.out.println();
+			
+			Scanner keyboard = new Scanner(System.in);
+			int choice = 0;
+			
+			while (choice<1 || choice >3)
+			{
+				System.out.print("Enter your choice [1-3]: ");
+				try
+				{
+					choice = keyboard.nextInt();
+				}
+				catch(NumberFormatException e)
+				{
+					System.out.println();
+					System.out.println("Must be a number.");
+				}
+			}
+			
+			System.out.println();
+			
+			List selectedList = listOptions.get(choice-1);
+			
+			if (selectedList==activeList)
+			{
+				System.out.println("Correct, "+a_or_an+" "+activeWord+" is a "+activeList.getName()+".");
+			}
+			else
+			{
+				System.out.println("Wrong, "+a_or_an+" "+activeWord+" is a "+activeList.getName()+".");
+			}
+			
+			System.out.println();
+			System.out.println("----------------------------------------------");
+			System.out.println();
+			
+		}
 		
 	}
 
